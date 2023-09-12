a4697492a343eca9a59361446ea321d30a52909d
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2023-09-12 14:33:37 -0700

    minor changes

--- src/components/TypingIndicator.jsx
+++ src/components/TypingIndicator.jsx
@@ -2,7 +2,7 @@ export default function TypingIndicator(props) {
   return (
     <div className={props.condition ? "" : "hide"}>
       <p>
-        <i>{props.condition ? "Typing" : ""}</i>
+        <i>{props.condition ? "Typing..." : ""}</i>
       </p>
     </div>
   );
